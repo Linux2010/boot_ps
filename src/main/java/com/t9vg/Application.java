358b2736564dd3f7d54b42294451ba399f5bdb3b
jdzw2018 <949465194@qq.com>
2018-12-21 15:43:06 +0800

    init

---
+++ src/main/java/com/t9vg/Application.java
@@ -0,0 +1,21 @@
+package com.t9vg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tianfusheng
+ * @version 1.0.0
+ * @项目名称: boot_ps
+ * @类名称: com.t9vg
+ * @类描述:
+ * @功能描述:
+ * @创建时间: 2018/12/21
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+
+}
